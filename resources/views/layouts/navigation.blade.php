36b1e811e9763c8ebfe2176385a16df08b0ecb55
MuhamadAzmiN <azmi89017@gmail.com>
2024-08-27 08:28:07 +0700

    test

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -85,10 +85,6 @@
   
 
 
-
-  {{--NAV LAMA >>>>  --}}
-
-
 {{-- <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="bg-indigo-600 dark:bg-gray-800 px-4 py-3 text-white dark:text-gray-200"">
